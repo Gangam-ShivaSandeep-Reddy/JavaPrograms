423d15a0732d5bd5f8f3047d75256c1ce0008767
Gangam-ShivaSandeep-Reddy <shivasandeepreddygangam@gmail.com>
2024-09-07 23:11:30 +0530

    Add files via upload

---
+++ Hibernate_CachingMechanismLevel2/src/main/java/com/jsp/Hibernate/Hibernate_CachingMechanismLevel2/App.java
@@ -0,0 +1,13 @@
+package com.jsp.Hibernate.Hibernate_CachingMechanismLevel2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
